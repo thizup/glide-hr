46245894d2bd61ce1c6bea9caa580125cf75690c
Thiago.Bertolino <thiago.bertolino@zup.com.br>
2023-02-28 19:29:24 -0300

    adding file for example

---
+++ main.ts
@@ -0,0 +1,18 @@
+import * as glide from "../glide";
+
+import Humanize from "humanize-plus";
+
+export default glide
+    .columnNamed("Abbreviate Number")
+    .withCategory("Number")
+    .withReleased("direct")
+    .withDescription(`Converts an integer into its most compact representation.`)
+    .withAuthor("HubSpot", "https://github.com/HubSpot/humanize")
+    .withStringResult()
+    .withRequiredNumberParam("number")
+    .withNumberParam("decimals", "Decimal Places")
+
+    .withTest({ number: 123456789 }, "123M")
+    .withTest({ number: 123456789, decimals: 2 }, "123.46M")
+
+    .run(({ number, decimals }) => Humanize.compactInteger(number, decimals));
